455343b34404c9a5f4560e0c2e7b5ec2c3436b77
William Harrison <william.harrison@engagency.com>
2022-01-05 14:03:55 -0600

    adds pages for things

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,12 @@
 export default {
-  projectRoot: '.',     // Where to resolve all URLs relative to. Useful if you have a monorepo project.
+  // projectRoot: '.',     // Where to resolve all URLs relative to. Useful if you have a monorepo project.
   // pages: './src/pages', // Path to Astro components, pages, and data
   // dist: './dist',       // When running `astro build`, path to final static output
   // public: './public',   // A folder of static files Astro will copy to the root. Useful for favicons, images, and other files that don’t need processing.
   buildOptions: {
-    site: 'https://wm-engagency.github.io/fed-stds/',           // Your public domain, e.g.: https://my-site.dev/. Used to generate sitemaps and canonical URLs.
-    sitemap: true,
-    pageUrlFormat: 'file',        // Generate sitemap (set to "false" to disable)
+    // site: 'https://wm-engagency.github.io/fed-stds/',           // Your public domain, e.g.: https://my-site.dev/. Used to generate sitemaps and canonical URLs.
+    // sitemap: true,
+    // pageUrlFormat: 'file',        // Generate sitemap (set to "false" to disable)
   },
   devOptions: {
     // hostname: 'localhost',  // The hostname to run the dev server on.
