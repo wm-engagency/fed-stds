0f3fef101b44f02db5332ff3c08c3b233323c368
William Harrison <william.harrison@engagency.com>
2022-01-05 12:08:36 -0600

    working on nav and page layouts

---
+++ src/config.ts
@@ -0,0 +1,20 @@
+export const SITE = {
+    title: 'Front End Development Standards & "Best" Practices',
+    description: 'These Front End Development Standards & "Best" Practices are a collection / amalgamation of others efforts to define what works best for them. Sources cited, edits ours.'
+};
+
+export const OPEN_GRAPH = {
+    image: {
+        src: '',
+        alt: '', 
+    },
+};
+
+export const GITHUB_EDIT_URL = ``;
+
+export const NAV = [
+    { text: 'HTML', link: '/html'},
+    { text: 'JavaScript', link: '/javascript'},
+    { text: 'SCSS', link: '/scss'},
+    { text: 'Git', link: '/git'},
+];
